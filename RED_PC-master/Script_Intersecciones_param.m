87b6ecfa60920d5a011ee781538ea43c8b6ed110
Ivana Velasco Alen <ivana.velasco@hotmail.com>
2014-12-28 12:21:54 -0600

    Script Intersecciones Parametrizado

---
+++ RED_PC-master/Script_Intersecciones_param.m
@@ -0,0 +1,39 @@
+clear all
+close all
+clc
+
+ciudad = 'CdOBREGON';
+grupo = '3';
+mat_ciudad = sprintf('MAT_%s',ciudad);
+nodes_ciudad = sprintf('nodes_%s',ciudad);
+dist_ciudad = sprintf('dist_%s',ciudad);
+nombre_matriz = sprintf('Matriz-%s-G%s.csv',ciudad,grupo);
+
+load(mat_ciudad)
+
+CSV_MATRIZ  = csvread(nombre_matriz,1,1 );
+nodes_MATRIZ  = CSV_MATRIZ(:,1);
+eval(sprintf('n_MATRIZ  = length(%s)',nodes_ciudad));
+
+dif_MATRIZ = zeros(n_MATRIZ,4);
+
+for i=1:n_MATRIZ
+    conteo=1;
+    for j1=1:n_MATRIZ
+        if CSV_MATRIZ(i,j1)~=0
+            loc1=CSV_MATRIZ(i,j1);
+            for j2=j1+1:n_MATRIZ
+                if CSV_MATRIZ(i,j2)~=0
+                    loc2=CSV_MATRIZ(i,j2);
+                    ab=loc1+loc2;
+                    eval(sprintf('c  = %s(j1,j2)',dist_ciudad));
+                    dif=(ab-c)/2;
+%                     dif_MATRIZCdOBREGON(i,j1)=dif;
+                    dif_MATRIZ(i,conteo)=dif;
+                    conteo=conteo+1;
+                end
+            end
+        end
+    end
+end
+save(sprintf('MATdif_%s-G%s.mat',ciudad,grupo),'dif_MATRIZ');
